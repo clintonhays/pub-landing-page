831a16f50d671148e059861bcda63b93e34d5553
clintonhays <clintonhays84@gmail.com>
2021-01-09 19:04:39 +0900

    add hamburger toggle to nav links

--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
 // Overlay Toggle
 
-// const overlay = document.getElementById("overlay");
+const overlay = document.getElementById("overlay");
 
-// overlay.addEventListener("click", (e) => {
-//   target = e.target;
-//   const html = `
-//     <div class="ageCheck">
-//       <h1>Please return when you have reached legal dinking age!</h1>
-//     </div>
-//   `;
+overlay.addEventListener("click", (e) => {
+  target = e.target;
+  const html = `
+    <div class="ageCheck">
+      <h1>Please return when you have reached legal dinking age!</h1>
+    </div>
+  `;
 
-//   if (target.className === "yes") {
-//     overlay.style.display = "none";
-//   } else {
-//     overlay.innerHTML = html;
-//   }
-// });
+  if (target.className === "yes") {
+    overlay.style.display = "none";
+  } else {
+    overlay.innerHTML = html;
+  }
+});
 
 // Hamburger Toggle
 
 const hamburger = document.querySelector(".hamburger");
 const navigation = document.querySelector(".navigation");
+const navLinks = document.querySelectorAll(".navItem");
 
 const hamburgerToggle = () => {
   navigation.classList.toggle("showNav");
@@ -28,3 +29,7 @@ const hamburgerToggle = () => {
 };
 
 hamburger.addEventListener("click", hamburgerToggle);
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", hamburgerToggle);
+});
