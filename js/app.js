26bfe458fb8ed67a9ee03929115dec7d4f7989e9
clintonhays <clintonhays84@gmail.com>
2021-01-09 16:28:46 +0900

    create hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
-// Global Variables
+// Overlay Toggle
 
-const overlay = document.getElementById("overlay");
+// const overlay = document.getElementById("overlay");
 
-overlay.addEventListener("click", (e) => {
-  target = e.target;
-  const html = `
-    <div class="ageCheck">
-      <h1>Please return when you have reached legal dinking age!</h1>
-    </div>
-  `;
+// overlay.addEventListener("click", (e) => {
+//   target = e.target;
+//   const html = `
+//     <div class="ageCheck">
+//       <h1>Please return when you have reached legal dinking age!</h1>
+//     </div>
+//   `;
 
-  if (target.className === "yes") {
-    console.log("working");
-    overlay.style.display = "none";
-  } else {
-    overlay.innerHTML = html;
-  }
-});
+//   if (target.className === "yes") {
+//     overlay.style.display = "none";
+//   } else {
+//     overlay.innerHTML = html;
+//   }
+// });
+
+// Hamburger Toggle
+
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
+
+const hamburgerToggle = () => {
+  navigation.classList.toggle("showNav");
+  hamburger.classList.toggle("hamburgerClose");
+};
+
+hamburger.addEventListener("click", hamburgerToggle);
