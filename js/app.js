f8352342d537554e6def5fefd99917fcae06e6b0
clintonhays <clintonhays84@gmail.com>
2021-01-10 21:13:47 +0900

    style sections and buttons, add footer, fix scroll on overlay

--- js/app.js
+++ js/app.js
@@ -1,21 +1,22 @@
 // Overlay Toggle
 
-// const overlay = document.getElementById("overlay");
+const overlay = document.getElementById("overlay");
 
-// overlay.addEventListener("click", (e) => {
-//   target = e.target;
-//   const html = `
-//     <div class="ageCheck">
-//       <h1>Please return when you have reached legal dinking age!</h1>
-//     </div>
-//   `;
+overlay.addEventListener("click", (e) => {
+  target = e.target;
+  const html = `
+    <div class="ageCheck">
+      <h1>Please return when you have reached legal dinking age!</h1>
+    </div>
+  `;
 
-//   if (target.className === "yes") {
-//     overlay.style.display = "none";
-//   } else {
-//     overlay.innerHTML = html;
-//   }
-// });
+  if (target.classList.contains("yes")) {
+    overlay.style.display = "none";
+    document.querySelector(".noScroll").classList.remove("noScroll");
+  } else if (target.classList.contains("no")) {
+    overlay.innerHTML = html;
+  }
+});
 
 // Hamburger Toggle
 
