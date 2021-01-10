8e765d70f55abd129dbf6fae9bbb99df02c265d4
clintonhays <clintonhays84@gmail.com>
2021-01-10 12:30:05 +0900

    add banner background animation, add view menu button

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 // Overlay Toggle
 
-const overlay = document.getElementById("overlay");
+// const overlay = document.getElementById("overlay");
 
-overlay.addEventListener("click", (e) => {
-  target = e.target;
-  const html = `
-    <div class="ageCheck">
-      <h1>Please return when you have reached legal dinking age!</h1>
-    </div>
-  `;
+// overlay.addEventListener("click", (e) => {
+//   target = e.target;
+//   const html = `
+//     <div class="ageCheck">
+//       <h1>Please return when you have reached legal dinking age!</h1>
+//     </div>
+//   `;
 
-  if (target.className === "yes") {
-    overlay.style.display = "none";
-  } else {
-    overlay.innerHTML = html;
-  }
-});
+//   if (target.className === "yes") {
+//     overlay.style.display = "none";
+//   } else {
+//     overlay.innerHTML = html;
+//   }
+// });
 
 // Hamburger Toggle
 
